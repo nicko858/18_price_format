7a9c0bbe7286932959bbd9c91e00f7e07d42100d
nicko858 <ikar2021@hotmail.com>
2018-08-09 12:26:36 +0300

    round to two decimal places, if there are no zeros.

--- format_price.py
+++ format_price.py
@@ -1,6 +1,7 @@
 import argparse
 from decimal import Decimal
 from decimal import InvalidOperation
+from decimal import ROUND_05UP
 
 
 def get_args():
@@ -20,14 +21,19 @@ def get_args():
 
 def format_price(price):
     try:
-        return '{:,}'.format(
-            Decimal(str(price)).normalize()
-        ).replace(',', ' ')
+        if float.is_integer(float(price)):
+            return "{:,}".format(
+                Decimal(str(price)).normalize()).replace(",", " ")
+        else:
+            return "{:,}".format(
+                Decimal(str(price)).quantize(
+                    Decimal("1.00"), ROUND_05UP)
+            ).replace(",", " ")
     except (ValueError, TypeError, InvalidOperation):
         return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_args()
     price = args.price
     print(format_price(price))
